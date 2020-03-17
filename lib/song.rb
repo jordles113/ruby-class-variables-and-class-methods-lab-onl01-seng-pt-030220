1a4432d882f176725f3ea85ca3d3fd4fe1eda157
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-17 22:48:12 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = []
+  @@artists
+  
+  def intitialize 
+    @@count += 1 
+    @@genres << 
+  end 
+  
+  def count 
+    @@count 
+  end 
+  
+  def genres 
+    @@genres 
+  end 
+  
+  def artists 
+    @@artists 
+  end 
+  
+  def genre_count
+    
+  end 
+end 
