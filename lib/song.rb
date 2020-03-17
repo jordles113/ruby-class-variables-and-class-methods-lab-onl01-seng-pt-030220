70ed12ce8c2e044de4fa685548c9c479c0a863d8
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-17 22:58:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,49 +1,54 @@
-class Song 
-  attr_accessor :name, :artist, :genre 
-  
-  @@count = 0 
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@song_count = 0
   @@genres = []
   @@artists = []
-  
-  def intitialize 
-    @@count += 1 
-     @name = name
+
+  def initialize(name, artist, genre)
+    @name = name
     @artist = artist
     @genre = genre
     @@genres << @genre
     @@artists << @artist
-  end 
-  
-  def self.count 
-    @@count 
-  end 
-  
-  def self.genres 
-    @@genres.uniq 
-  end 
-  
-  def self.artists 
-    @@artists.uniq 
-  end 
-  
+    @@song_count +=1
+  end
+
+  def self.count
+    @@song_count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
   def self.genre_count
-    genre_count = {} 
-    @@genres.each do |genre|
+    genre_count = {}
+    @@genres.each do |genre| 
       if genre_count[genre]
-        genre_count[genre] += 1 
-      else 
-        genre_count = 1 
-      end 
-      genre_count
-  end 
-  
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
   def self.artist_count
     artist_count = {}
-    @@artitists.each do |artist| 
+    @@artists.each do |artist|
       if artist_count[artist]
-        artist_count[artist] += 1 
-      else 
-        artist_count = 1 
-      end 
-      artist_count
-  end 
+      artist_count[artist] +=1
+      else
+      artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+
+end
